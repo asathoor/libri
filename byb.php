8c43eab8ec3a15b6d479d976e6fe284cfd652374
asathoor <per@multimusen.dk>
2015-12-15 23:50:24 +0100

    tex two: php version

---
+++ byb.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * file: byb.php
+ * purpose: format litterature list as tex / bybliographic database.
+*/
+
+header("Content-type: text/html; charset=iso-8859-1"); // danish etc. charset
+
+include_once "db.php"; // database connection ($mysqli)
+
+$sql = "SELECT * FROM `libri` order by `Author`";
+
+$result = $mysqli->query($sql);
+
+while($row = $result->fetch_assoc()){
+     print "\n@Book{" . strtoupper( substr($row['Author'], 0,4)) . "_" . $row['Year']
+     . ", Author={" . $row['Author'] . "},\n"
+     . "Title={" . $row['Title'] . "},\n"
+     . "Publisher={" . $row['Where'] . "},\n"
+     . "Year=" . $row['Year'] . "}\n";
+   }
+?>
